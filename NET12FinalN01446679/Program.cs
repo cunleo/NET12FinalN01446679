f3ecd88e74d5c86035f0a5785e7eb60420612bf9
OLAPOJU OLUWOLE <35400817+cunleo@users.noreply.github.com>
2020-07-02 20:43:43 -0400

    Add project files.

---
+++ NET12FinalN01446679/Program.cs
@@ -0,0 +1,50 @@
+﻿using System;
+
+namespace NET12FinalN01446679
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Person[] person = new Person[] { new Adult(), new Child() };
+
+
+           foreach(Person operson in person)
+            {
+                Console.WriteLine(operson.firstName);
+                Console.WriteLine(operson.lastName);
+                Console.ReadLine();
+            }
+
+        }
+        class Person
+        {
+            public String firstName { get; set; }
+            public String lastName { get; set; }
+        }
+        class Adult: Person
+        {
+            public String firstName { get; set; }
+            public String lastName { get; set; }
+
+
+            public Adult() //string firstname, String lastname)
+            {
+                base.firstName = "Smith";
+                base.lastName = "James";
+            }
+
+        }
+        class Child: Person
+        {
+            public String firstName { get; set; }
+            public String lastName { get; set; }
+
+            public Child() //string firstname, String lastname)
+            {
+                base.firstName = "Adams";
+                base.lastName = "Jack";
+            }
+        }
+    }
+}
